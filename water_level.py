3ec1a78780ea522d217fba9868aebfa3a02d1cfb
Kevin Beeson <klbeeson@gmail.com>
2025-04-01 15:39:23 +0100

    Added stream lit functionality to the water level

--- water_level.py
+++ water_level.py
@@ -1,3 +1,4 @@
+# Import necessary libraries
 import requests
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -5,142 +6,144 @@ import matplotlib.dates as mdates
 import logging
 import matplotlib 
 import copy
+import streamlit as st
+import numpy as np
+
+# Set matplotlib backend
 matplotlib.use('Qt5Agg')
+
+# Set logging level to INFO
 logging.getLogger().setLevel(logging.INFO)
+
+# Set pandas display options
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 
 
+# Main Streamlit app starts here
 if __name__ == '__main__':
-    #get all the stations
-    logging.info("Getting all the stations")
-    response = requests.get("https://environment.data.gov.uk/flood-monitoring/id/stations")
-    if response.status_code != 200:
-        logging.error("Failed to get stations")
-        exit(1)
-    else:
-        logging.info("Successfully got stations")
-    
+    # Define parameters for filtering station data
+    parameters = ['RLOIid', 'town', 'catchmentName', 'riverName', 'label']
 
-    #process the data into a pandas dataframe
-    data=response.json()
-    stations=data['items']
-    data=pd.DataFrame(stations)
-    id=[x.split('/')[-1] for x in data['@id']]
-    data['id_name']=id
+    # Check if data already in session; if not, fetch station data from API
+    if 'data' not in st.session_state:
+        logging.info("Getting all the stations")
+        st.write("Getting all the stations")
 
-    #selects the station based on user input
-    parameters=['id_name','town','catchmentName','riverName','label']
-    data_original=copy.deepcopy(data)
-    while len(data)>1:
-        logging.info("Displaying the first 20 stations")
-        logging.info("\n"+data[parameters][0:20].to_markdown())
-        logging.info("There are "+str(len(data))+" stations")
-        logging.info("Which station would you like to select? Enter parameter=parameter_value (e.g. town=Reading). If you would like to restart the selection, enter 'restart'")
-        logging.info("The available parameters are:"+str(parameters))
-        selected_station=input()
-        if any(x in selected_station for x in parameters) and '=' in selected_station:
-            parameter,parameter_value=selected_station.split('=')
-            parameter_value=parameter_value.lower()
-
-            #Create a mask for the selected parameter
-            mask=[x.lower()==parameter_value if type(x)==str else x==parameter_value for x in data[parameter]]
-            if sum(mask)==0:
-                logging.info("No station with the specified parameter")
-            else:
-                data=data[mask]
-                data.reset_index(drop=True,inplace=True)
-        elif selected_station=='restart':
-            data=copy.deepcopy(data_original)
+        response = requests.get("https://environment.data.gov.uk/flood-monitoring/id/stations")
+        if response.status_code != 200:
+            logging.error("Failed to get stations")
+            st.write("Failed to get stations")
+            exit(1)
         else:
-            logging.info("Invalid input")
-    
-        
+            logging.info("Successfully got stations")
+            st.write("Successfully got stations")
 
+        # Convert API response to DataFrame
+        stations = response.json()['items']
+        data = pd.DataFrame(stations)
 
-    unit_name=data['measures'][0][0]['unitName']
-    example_station_id = data['@id'][0]
-    logging.info("Getting the data for the station with id: "+example_station_id.split('/')[-1])
-    response = requests.get(example_station_id+"/readings?_sorted")
-    if response.status_code != 200:
-        logging.error("Failed to get for the station")
-        sys.exit(1)
+        # Ensure specific columns are strings
+        for x in parameters:
+            data[x] = data[x].astype(str)
+
+        # Cache data in Streamlit session
+        st.session_state.data = data
+        st.session_state.data_original = copy.deepcopy(data)
     else:
-        logging.info("Successfully got for the station")
-    data_station=response.json()
-    readings=data_station['items']
-    data_station=pd.DataFrame(readings)
+        logging.info("Using cached data")
+        st.write("Using cached data")
+        data = st.session_state.data
 
-    #get the last measurement for the day 
-    data_station['dateTime'] = pd.to_datetime(data_station['dateTime'])
+    # Streamlit UI: title and subheader
+    st.title("Water Level Monitoring")
+    st.subheader("Select a station to monitor water levels")
 
-    #get the types of measurements
-    types=data_station['measure'].unique()
-    if len(types)>1:
-        logging.info("The types of measurements are: ")
-        #print types with an index
-        logging.info("Index Type")
-        for i in range(len(types)):
-            logging.info((i,types[i]))
-        logging.info("Which type(s) of measurement would you like to plot? select the relevant index number, if multiples types separate are by a comma (e.g. 0,1)")
-        selection=True
-        while selection:
-            selected_types=input()
-            selected_types=selected_types.split(',')
-            try:
-                for i in range(len(selected_types)):
-                    selected_types[i]=types[int(selected_types[i])]
-                logging.info("Selected types of measurements are: ") 
-                logging.info(selected_types)
-                selection=False
-            except:
-                logging.info("Invalid input")
-                logging.info("Which type(s) of measurement would you like to plot? select the relevant index number, if multiples types separate by a comma (e.g. 0,1)")
-    else:
-        logging.info("The type of measurement is: "+types[0])
-        selected_types=[types[0]]
+    # Add reset button to clear filters
+    if st.button("Reset"):
+        logging.info("Resetting the selection")
+        st.session_state.data = copy.deepcopy(st.session_state.data_original)
+        if 'data_station' in st.session_state:
+            del st.session_state['data_station']
 
-    #create mask for the selected types
-    mask = data_station['measure'].isin(selected_types)
-    data_station = data_station[mask]
+    # Create dropdown filters for each parameter
+    filtered_data = st.session_state.data.copy()
+    make_choice = {}   
+    for value, name in enumerate(parameters):
+        options = [None] + sorted(filtered_data[name].explode().dropna().unique().tolist())
+        choice = st.selectbox(f"Select {name}", options)
+        make_choice[name] = choice
 
-    #filter the data for the last 24 hours
-    current_time = pd.Timestamp.now()
-    current_time = current_time.tz_localize('UTC')
-    # Use loc to filter for rows within the last 24 hours
-    data_station_filtered = data_station.loc[data_station['dateTime'] >= (current_time - pd.Timedelta(hours=24))]
+        if choice is not None:
+            filtered_data = filtered_data[filtered_data[name] == choice]
+            filtered_data.reset_index(drop=True, inplace=True)
 
-    # Alternatively, you can use mask:
-    data_station_filtered = data_station_filtered.mask(data_station_filtered['dateTime'] < (current_time - pd.Timedelta(hours=24)))
-    
-    logging.info("\n"+data_station_filtered[['dateTime','value','measure']].to_markdown())
-    #plot the data using matplotlib
+    # Show the filtered data
+    st.session_state.data = filtered_data
+    st.write(filtered_data[parameters])
 
-    ax, fig = plt.subplots()
-    for measurement_type in selected_types:
-        data_station_filtered_type = data_station_filtered.loc[data_station_filtered['measure'] == measurement_type]
-        type_name = measurement_type.split('/')[-1]
-        plt.plot(data_station_filtered_type['dateTime'], data_station_filtered_type['value'], label=type_name)
-    plt.legend()
-    plt.xlabel('Time')
-    plt.ylabel('Value /'+unit_name)
-    station_name=example_station_id.split('/')[-1]
-    plt.title('Last 24 hours of data at station '+station_name)
+    # If only one station is selected, fetch and display its readings
+    if len(filtered_data) == 1:
+        logging.info("Displaying the selected station")
 
-    # Set major and minor ticks format
-    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
-    plt.gca().xaxis.set_minor_formatter(mdates.DateFormatter(''))
+        unit_name = filtered_data['measures'][0][0]['unitName']
+        example_station_id = filtered_data['@id'][0]
+        logging.info("Getting the data for station: " + example_station_id.split('/')[-1])
 
-    # Set major and minor ticks locator
-    plt.gca().xaxis.set_major_locator(mdates.HourLocator(interval=1))
-    plt.gca().xaxis.set_minor_locator(mdates.MinuteLocator(interval=15))
+        # Fetch station reading data if not already cached
+        if 'data_station' not in st.session_state:
+            logging.info("Getting the data for the station")
+            st.write("Getting the data for the station")
+            response = requests.get(example_station_id + "/readings?_sorted")
+            if response.status_code != 200:
+                logging.error("Failed to get data for the station")
+                sys.exit(1)
 
-    # Align the minor ticks with the major ticks
-    plt.gca().xaxis.set_minor_locator(mdates.MinuteLocator(byminute=range(0, 60, 15)))
+            data_station = response.json()['items']
+            data_station = pd.DataFrame(data_station)
+            data_station['dateTime'] = pd.to_datetime(data_station['dateTime'])
+            st.session_state.data_station = data_station
+        else:
+            logging.info("Using cached data for the station")
+            st.write("Using cached data for the station")
+            data_station = st.session_state.data_station
 
-    plt.gcf().autofmt_xdate()  # Rotation of x-axis labels for better readability
+        # Show measurement types and allow user to select
+        types = data_station['measure'].unique()
+        selected_types = st.multiselect("Select the type of measurement", types)
 
-    plt.show()
+        if selected_types:
+            logging.info(f"Filtering data by measure = {selected_types}")
 
+            # Filter by selected measure types
+            mask = data_station['measure'].isin(selected_types)
+            data_station = data_station[mask]
 
+            # Filter data for the last 24 hours
+            current_time = pd.Timestamp.now().tz_localize('UTC')
+            data_station_filtered = data_station.loc[data_station['dateTime'] >= (current_time - pd.Timedelta(hours=24))]
 
+            st.write(data_station_filtered)
+
+            # Plot the filtered data
+            ax, fig = plt.subplots()
+            for measurement_type in selected_types:
+                type_data = data_station_filtered[data_station_filtered['measure'] == measurement_type]
+                type_name = measurement_type.split('/')[-1]
+                plt.plot(type_data['dateTime'], type_data['value'], label=type_name)
+
+            plt.legend()
+            plt.xlabel('Time')
+            plt.ylabel('Value / ' + unit_name)
+            station_name = example_station_id.split('/')[-1]
+            plt.title(f'Last 24 hours of data at station {station_name}')
+
+            # Configure the x-axis date formatting
+            plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
+            plt.gca().xaxis.set_minor_formatter(mdates.DateFormatter(''))
+            plt.gca().xaxis.set_major_locator(mdates.HourLocator(interval=1))
+            plt.gca().xaxis.set_minor_locator(mdates.MinuteLocator(byminute=range(0, 60, 15)))
+            plt.gcf().autofmt_xdate()
+
+            # Render the plot in Streamlit
+            st.pyplot(ax)
